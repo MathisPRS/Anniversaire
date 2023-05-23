7a317308b2ce3e101e83df95f5226dca2ba60e8f
Mathis <mathis.reclus@gmail.com>
2023-05-23 17:33:59 +0200

    FRONT css

--- anniversaire-front/src/app/navbar/navbar.component.ts
+++ anniversaire-front/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SidebarService } from '../services/sidebar.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  showSidebar = false;
+  showProfileMenu = false ;
 
+  constructor(private router: Router, private sidebarService: SidebarService) { }
+
+  toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
+    this.sidebarService.updateSidebar(this.showSidebar);
+  }
+  toggleProfileMenu() {
+    this.showProfileMenu = !this.showProfileMenu;
+  }
+    onLogout() {
+    localStorage.removeItem('access_token');
+    this.router.navigate(['/login']);
+  }
+  checkprofile() {
+    this.router.navigate(['/monprofile']);
+  }
+  
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const profileIcon = document.querySelector('.profile-icon');
+
+    if (profileIcon && !profileIcon.contains(target)) {
+      this.showProfileMenu = false;
+    }
+  }
 }
